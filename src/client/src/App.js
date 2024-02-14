d330f04abcd5a1d0a0375bced6d760b381b03a2a
Dhruvadeep Malakar <dhruvadeepmalakar12345@gmail.com>
2024-02-14 14:00:33 +0530

    testing for react app - ignore

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,15 +1,23 @@
-import { useState, useEffect, useRef } from 'react';
-import './App.css';
-import axios from 'axios'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './scss/main.css'
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import axios from "axios";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./scss/main.css";
 
 function App() {
-
   return (
-    <div className='container btn' > Hello world </div>
+    <>
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1>Test</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-
 }
 
 export default App;
